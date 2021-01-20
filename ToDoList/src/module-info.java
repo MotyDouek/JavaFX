c677c981d36d8bb44101ab5bdfae758341742316
Moty Douek <49308633+MotyDouek@users.noreply.github.com>
2021-01-20 10:46:47 +0200

    adding media

--- ToDoList/src/module-info.java
+++ ToDoList/src/module-info.java
@@ -2,5 +2,9 @@ module ToDoList {
     requires javafx.fxml;
     requires javafx.controls;
 
+    requires javafx.media;
+    requires javafx.graphics;
+    requires javafx.web;
+
     opens todolist;
 }
